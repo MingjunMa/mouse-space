c28335094c959bf55dcdac1c744471431b6696a3
mingjun <imingjun.ma@outlook.com>
2022-04-20 23:24:51 +0800

    feat: change gitignore and initial project

---
+++ src/main/java/com/mouse/mousespace/MouseSpaceApplication.java
@@ -0,0 +1,13 @@
+package com.mouse.mousespace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MouseSpaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MouseSpaceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mouse/mousespace/MouseSpaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mouse.mousespace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MouseSpaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
